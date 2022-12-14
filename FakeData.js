1b743029642da8210716cca42b2306b7e89c73c6
Johanna Wirell <jw223gx@student.lnu.se>
2022-12-14 11:01:31 +0100

    Inital commit

---
+++ FakeData.js
@@ -0,0 +1,22 @@
+export const users = [
+  {
+    name: "Pedro",
+    age: 19,
+    married: false
+  },
+   {
+    name: "Pedro",
+    age: 15,
+    married: true
+  },
+   {
+    name: "Angela",
+    age: 20,
+    married: false
+  },
+   {
+    name: "Jessica",
+    age: 25,
+    married: true
+  }
+]
